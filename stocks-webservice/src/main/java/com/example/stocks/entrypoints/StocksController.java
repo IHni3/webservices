b12265f444043b8d4f51291c56a4444804cb5555
Tim Hönnige <timhoennige@gmx.de>
2021-02-09 20:20:04 +0100

    updated service

--- stocks-webservice/src/main/java/com/example/stocks/entrypoints/StocksController.java
+++ stocks-webservice/src/main/java/com/example/stocks/entrypoints/StocksController.java
@@ -1,11 +1,18 @@
 package com.example.stocks.entrypoints;
 
+import com.example.stocks.core.domains.Stock;
 import com.example.stocks.core.usecases.CreateStockUseCase;
 import com.example.stocks.core.usecases.DeleteStockUseCase;
 import com.example.stocks.core.usecases.GetStocksUseCase;
 import com.example.stocks.dataproviders.persistence.jpa.JpaStocksDataProvider;
 import com.example.stocks.dataproviders.persistence.jpa.StocksRepository;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 @RestController
 public class StocksController {
@@ -23,4 +30,30 @@ public class StocksController {
         deleteStockUseCase = new DeleteStockUseCase(dataProvider);
         getStocksUseCase = new GetStocksUseCase(dataProvider);
     }
+
+    @RequestMapping(value = "/stock")
+    public ResponseEntity<List<Stock>> getAll()
+    {
+        List<Stock> personList = getStocksUseCase.findAll();
+        return new ResponseEntity<>(personList, HttpStatus.OK);
+    }
+
+    @RequestMapping(value = "/stock/{id}", method = RequestMethod.DELETE)
+    public ResponseEntity<Stock> delete(@PathVariable("id") UUID id)
+    {
+        Optional<Stock> stockOptional = getStocksUseCase.findById(id);
+        if(stockOptional.isPresent()) {
+            deleteStockUseCase.deleteStock(id);
+            return new ResponseEntity<>(HttpStatus.OK);
+        }
+        else
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+    @RequestMapping(value = "/stock", method = RequestMethod.PUT)
+    public ResponseEntity<Stock> put(@RequestBody Stock stock)
+    {
+        Stock savedPerson = createStockUseCase.create(stock);
+        return new ResponseEntity<>(savedPerson, HttpStatus.OK);
+    }
+
 }
