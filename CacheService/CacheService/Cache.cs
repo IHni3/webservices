ded36540b5a5ea03b9ffac357c95b212e0769018
GeorgeKl97 <72011240+GeorgeKl97@users.noreply.github.com>
2021-02-12 13:50:26 +0100

    CacheService
    CacheService with Swagger now in the develop Branch

---
+++ CacheService/CacheService/Cache.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.Extensions.Caching.Memory;
+
+namespace CacheService
+{
+    public class Cache<TItem>
+    {
+
+        // Setting the size limit of the cache.
+        private static MemoryCache _cache = new MemoryCache(new MemoryCacheOptions()
+        {
+            SizeLimit = 1024
+        });
+
+        public static TItem GetOrCreate(object key, Func<TItem> createItem)
+        {
+            TItem cacheEntry;
+            if (!_cache.TryGetValue(key, out cacheEntry))// Look for cache hashkey.
+            {
+                // Hashkey not in cache, so get data.
+                cacheEntry = createItem();
+
+                var cacheEntryOptions = new MemoryCacheEntryOptions()
+                 .SetSize(1)//Size amount
+                            //Priority on removing when reaching size limit (memory pressure)
+                    .SetPriority(CacheItemPriority.High)
+                    // Keep in cache for this time, reset time if accessed.
+                    .SetSlidingExpiration(TimeSpan.FromSeconds(30))
+                    // Remove from cache after this time, regardless of sliding expiration
+                    .SetAbsoluteExpiration(TimeSpan.FromMinutes(5));
+
+                // Save data in cache.
+                _cache.Set(key, cacheEntry, cacheEntryOptions);
+            }
+            return cacheEntry;
+        }
+
+
+    }
+}
---
+++ CacheService/CacheService/Controllers/CacheItemsController.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using CacheService.Models;
+using System.Net;
+using System.IO;
+using Newtonsoft.Json;
+
+namespace CacheService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CacheItemsController : ControllerBase
+    {
+        private readonly CacheContext _context;
+
+        public CacheItemsController(CacheContext context)
+        {
+            _context = context;
+        }
+
+        /* POST: api/CacheItems
+           Structure of the GET request
+            "URL":"URL from asked API",
+            "Querry":"Querry from Asked API",
+            "Awnser":"" - Should be left blank - 
+
+          To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+
+          Function to look up a querry in the cache. 
+          If the awnser is blank it forwards it to the asked API for a an awnser,
+          else it just returnes the information from the cache
+
+        Needs to be Post, or else Swagger won't look for parameters in Body.
+        */
+        [HttpPost]
+        public async Task<ActionResult<String>> GetCacheItem(CacheItem cacheItem)
+        {
+            if (cacheItem.URL.Equals(null) || cacheItem.Querry.Equals(null))
+            {
+                throw new ArgumentNullException("CacheItem was null");
+            }
+            else
+            {
+                int hash = (cacheItem.URL.GetHashCode()) + (cacheItem.Querry.GetHashCode());              
+                var querry = Cache<CacheItem>.GetOrCreate(hash, () => cacheItem);
+                if (querry.Awnser.Equals(""))
+                {
+                    cacheItem.Awnser = Get(cacheItem.URL);
+                    querry = Cache<CacheItem>.GetOrCreate(hash, () => cacheItem);
+                }
+                return querry.Awnser;
+            }
+        }
+
+        // Function to forward the querry if it isn't in the cache
+        [HttpPost, Route("Throw")]
+        public string Get(string uri)
+        {
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(uri);
+            request.AutomaticDecompression = DecompressionMethods.GZip | DecompressionMethods.Deflate;
+
+            using (HttpWebResponse response = (HttpWebResponse)request.GetResponse())
+            using (Stream stream = response.GetResponseStream())
+            using (StreamReader reader = new StreamReader(stream))
+            {
+                return reader.ReadToEnd();
+            }
+        }
+    }
+}
