7b1e7687b86566895b1e4519a9725e96daed573c
GeorgeKl97 <72011240+GeorgeKl97@users.noreply.github.com>
2021-03-14 11:56:56 +0100

    new Glue

---
+++ Glue/Glue/GlueService/Program.cs
@@ -0,0 +1,37 @@
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
+using System;
+
+namespace Glue
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static String GetApiKey()
+        {
+            // Key for realease
+            string apiKey = "BBWFMJV26IKOX2AJ";
+            // Key for testing
+            //string apiKey = "demo";
+            return apiKey;
+        }
+
+        public static String GetCacheServiceURI()
+        {
+            string cacheServiceURI = "http://localhost:5001/";
+            //string cacheServiceURI = "https://localhost:44336/";
+            return cacheServiceURI;
+        }
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                   // webBuilder.UseUrls("http://localhost:5003");
+                });
+    }
+}
