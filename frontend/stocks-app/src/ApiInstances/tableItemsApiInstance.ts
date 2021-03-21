e5a11d8da56b9491721f1664f48ab54bf80d3594
Tim Hönnige <timhoennige@gmx.de>
2021-03-21 15:56:04 +0100

    added frontend to docker compose

--- frontend/stocks-app/src/ApiInstances/tableItemsApiInstance.ts
+++ frontend/stocks-app/src/ApiInstances/tableItemsApiInstance.ts
@@ -1,7 +1,7 @@
 import { Configuration, TableItemsApi } from "../GlueApi";
 
 let config = new Configuration();
-config.basePath = "http://localhost:4002";
+config.basePath = "http://hostess:4002";
 
 let api = new TableItemsApi(config);
 
